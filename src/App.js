1a16f09a739265bdf8d0b7c90678f6b624190f4b
Minghua Sun <clairesunstudio@gmail.com>
2017-07-19 10:33:30 -0400

    style app div

--- src/App.js
+++ src/App.js
@@ -10,24 +10,28 @@ import Dashboard_9 from './Dashboard_9'
 import Story from './Story'
 import Header from './Header'
 import Footer from './Footer'
+import './App.css'
 
 const App = () => (
     <Router>
-      <div>
-        <Story />
-        <ul className="nav nav-tabs nav-justified">
-          <li className="nav-item"><NavLink className="nav-link active"  to={`/9`}>9th Grade</NavLink></li>
-          <li className="nav-item"><NavLink className="nav-link"         to={`/10`}>10th Grade</NavLink></li>
-          <li className="nav-item"><NavLink className="nav-link"         to={`/11`}>11th Grade</NavLink></li>
-          <li className="nav-item"><NavLink className="nav-link"         to={`/12`}>12th Grade</NavLink></li>
-        </ul>
+      <div className="wrapper">
         <Header />
+        <div className="container">
+          <Story />
+          <ul className="nav nav-tabs nav-justified">
+            <li className="nav-item"><NavLink className="nav-link active"  to={`/9`}>9th Grade</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link"         to={`/10`}>10th Grade</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link"         to={`/11`}>11th Grade</NavLink></li>
+            <li className="nav-item"><NavLink className="nav-link"         to={`/12`}>12th Grade</NavLink></li>
+          </ul>
 
-        <Redirect from="/" to="/9"/>
-          <Route path={`/9`} component={Dashboard_9}/>
-          <Route path={`/(10|11|12)`} component={Dashboard}/>
+          <Redirect from="/" to="/9"/>
+            <Route path={`/9`} component={Dashboard_9}/>
+            <Route path={`/(10|11|12)`} component={Dashboard}/>
+        </div>
         <Footer />
       </div>
+
     </Router>
 
   )
