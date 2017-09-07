f568ebb7b6fb200e05edfd7e8c76cc139e34728b
Minghua Sun <clairesunstudio@gmail.com>
2017-09-07 13:35:32 -0400

    #6 click menu button to expand on mobile select a value to collapse

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,28 +1,46 @@
 import React from 'react'
 import { NavLink} from 'react-router-dom'
 import './Navigation.css'
+import Btn from './Btn'
 
 class Navigation extends React.Component {
   constructor(props, context){
     super(props, context);
     this.state = {
-      grade: '9th Grade'
+      grade: '9th Grade',
+      value: 'navbar-collapse'
     }
   }
+  handleClick = (e) =>  {
+    e.preventDefault();
+    console.log(e.target.innerText);
+    this.setState({
+      grade : e.target.innerText,
+      value : 'navbar-collapse'
+    })
+
+  };
+
+  toggleClick = (e) =>  {
+    e.preventDefault();
+    console.log(e.target.value)
+    this.setState({value : 'navbar-expand'})
+  };
+
   render() {
+    const { grade, value } = this.state
     return (
         <nav className="container navbar navbar-inverse" role="navigation" id="navigation">
             <div className="navbar-header">
-              <span className="navbar-brand" id="current-section">{this.state.grade}</span>
-              <button type="button" className="btn btn-inverse navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">Menu <i className="glyphicon glyphicon-menu-hamburger" aria-hidden="true" />
-              </button>
+              <span className="navbar-brand" id="current-section">{grade}</span>
+              <Btn handleClick={this.toggleClick} value={'navbar-expand'} selected={value} className="btn-inverse navbar-toggle">Menu <i className="glyphicon glyphicon-menu-hamburger" aria-hidden="true" /></Btn>
             </div>
-            <div className="navbar-collapse navbar-main-collapse">
+            <div className={value}>
               <ul className="nav nav-tabs nav-justified flex-column">
-                <li className="nav-item"><NavLink className="nav-link"  to={`/9`}>9th Grade</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link"         to={`/10`}>10th Grade</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link"         to={`/11`}>11th Grade</NavLink></li>
-                <li className="nav-item"><NavLink className="nav-link"         to={`/12`}>12th Grade</NavLink></li>
+                <li className="nav-item"  onClick={this.handleClick} ><NavLink className="nav-link" to={`/9`}>9th Grade</NavLink></li>
+                <li className="nav-item"  onClick={this.handleClick} ><NavLink className="nav-link" to={`/10`}>10th Grade</NavLink></li>
+                <li className="nav-item"  onClick={this.handleClick} ><NavLink className="nav-link" to={`/11`}>11th Grade</NavLink></li>
+                <li className="nav-item"  onClick={this.handleClick} ><NavLink className="nav-link" to={`/12`}>12th Grade</NavLink></li>
               </ul>
             </div>
         </nav>
