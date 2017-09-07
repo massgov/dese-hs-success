a484425f9c09bdbc4e66bc46385f96cb1e3dca19
Minghua Sun <clairesunstudio@gmail.com>
2017-09-07 14:17:51 -0400

    style Narrative list and move styles from App.scss to Narrative.scss

--- src/Narrative.js
+++ src/Narrative.js
@@ -22,9 +22,9 @@ class Content extends React.Component{
           <div className="col-md-12">
             <p>We followed a cohort of students through their 4-year journey in high school and  looked at the how students who met certain indicators compare to those who did not. It highlights that the ABCs do matter:</p>
             <ul>
-              <li><i className="glyphicon glyphicon-education color-primary" aria-hidden="true" /> A student who passes all grade 9 courses is more likely to graduate high school in 4 years than a student who fails one or more courses in ninth grade.</li>
-              <li><i className="glyphicon glyphicon-education color-primary" aria-hidden="true" /> Students who are chronically absent (miss more than 10% of days) are less likely graduate than those with high attendance.</li>
-              <li><i className="glyphicon glyphicon-education color-primary" aria-hidden="true" /> Students who take higher level coursework (higher level math courses or Advanced Placement courses) are more likely to go to and stay in college.</li>
+              <li><i className="glyphicon glyphicon-education list-icon" aria-hidden="true" /> A student who passes all grade 9 courses is more likely to graduate high school in 4 years than a student who fails one or more courses in ninth grade.</li>
+              <li><i className="glyphicon glyphicon-education list-icon" aria-hidden="true" /> Students who are chronically absent (miss more than 10% of days) are less likely graduate than those with high attendance.</li>
+              <li><i className="glyphicon glyphicon-education list-icon" aria-hidden="true" /> Students who take higher level coursework (higher level math courses or Advanced Placement courses) are more likely to go to and stay in college.</li>
             </ul>
           </div>
         </div>
