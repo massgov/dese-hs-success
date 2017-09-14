387de953b813663514b9aa67a0e83a0db2cf5ff8
Minghua Sun <clairesunstudio@gmail.com>
2017-09-14 17:29:37 -0400

    refactor linechart custom parts

---
+++ src/DashViz/LineChartCustomParts.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import {Dot} from 'recharts';
+
+
+export const CustomizedXAxisTick = ({x, y, stroke, payload}) => {
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={15} textAnchor="middle" fill="#666" fontSize={12} transform="rotate(0)">{payload.value}</text>
+      </g>
+    );
+}
+
+export const CustomizedYAxisTick = ({x, y, stroke, payload}) => {
+    return (
+        <g transform={`translate(${x},${y})`}>
+          <text x={0} y={0} dx={-5} dy={5} textAnchor="end" fill="#666" fontSize={12} transform="rotate(0)">{payload.value}%</text>
+        </g>
+    );
+}
+
+var value;
+export const CustomTooltip = (props) => {
+  const { payload } = props;
+  if (props.active) {
+    const { payload } = props;
+    if( payload[0].value != 'Null' ){
+      value = payload[0].value + '%'
+    } else { value = payload[0].value + '*'}
+    if(props.lastData == payload[0].value && props.lastYear == payload[0]['payload']['Yr']) {
+      return null;
+    } else {
+      return (
+        <div className='custom-tooltip-district'>
+          <text className='descr_value'>{`${(value)}`}</text>
+        </div>
+      );
+    }
+  }
+  return null;
+}
+
+export const CustomizedLabel = ({index, lastIndex, x, y, lastData}) => {
+    if (index == lastIndex) {
+        x = x;
+        y = y;
+        value = lastData + '%';
+    } else {
+      x = 0
+      y = 0
+      value = ''
+    }
+    return <text x={x} y={y} dy={-11} dx={5} fill={'#14558f'} fontSize={15} textAnchor='middle'>{value}</text>
+}
+
+export const CustomizedDot = ({cx, cy, stroke, fill, r, payload, value, index, lastIndex}) => {
+    if (lastIndex === index) {
+      return (
+        <Dot cx={cx} cy={cy} stroke={stroke} r={r+2} fill={'#14558f'} />
+      );
+    }
+    return (
+      <Dot cx={cx} cy={cy} r={r} stroke={stroke} fill={'#fff'} />
+    );
+}
