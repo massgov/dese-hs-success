cb053fc1123b02c382893aeed45f7dde3c4e61db
Sienna Svob <sienna.avril.pearl@gmail.com>
2017-09-14 11:30:26 -0400

    highlight last data point, only hover on non-last
    -highlight the last data point by default
    -hover tooltip only displayed if not hovering on the last data point

--- src/DashViz/LineChart.js
+++ src/DashViz/LineChart.js
@@ -1,10 +1,8 @@
-//import d3 from 'd3'
-import PropTypes from 'react'
-import React from 'react'
-import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Text, Dot} from 'recharts'
+import PropTypes from 'react';
+import React from 'react';
+import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Text, Dot} from 'recharts';
 
-var value, x, y, index, xTick, active;
-var lastFill = '#14558f' 
+var value, x, y, index, xTick, active, year;
 
 const CustomizedXAxisTick = React.createClass({
   render () {
@@ -21,12 +19,19 @@ const CustomizedYAxisTick = React.createClass({
   render () {
     const {x, y, stroke, payload} = this.props;
     return (
-      <g transform={`translate(${x},${y})`}>
-        <text x={0} y={0} dx={-5} dy={5} textAnchor="end" fill="#666" transform="rotate(0)">{payload.value}%</text>
-      </g>
+        <g transform={`translate(${x},${y})`}>
+          <text x={0} y={0} dx={-5} dy={5} textAnchor="end" fill="#666" transform="rotate(0)">{payload.value}%</text>
+        </g>
     );
   }
 });
+
+const CustomActiveDot = (props) => {
+  const { index, cx, cy, fill, r, value} = props
+  return (
+    <Dot cx={cx} cy={cy} r={r} fill={fill} />
+  )
+};
 
 const CustomizedLabel = React.createClass({
   render () {
@@ -39,34 +44,44 @@ const CustomizedLabel = React.createClass({
       y = 0
       value = ''
     } 
-    return <text x={x} y={y} dy={-10} dx={5} fill={lastFill} fontSize={15} textAnchor='middle'>{value}</text>
+    return <text x={x} y={y} dy={-11} dx={5} fill={'#14558f'} fontSize={15} textAnchor='middle'>{value}</text>
   }
 });
 
 export function CustomTooltip(props) {
   const { payload } = props;
   if (props.active) {
-    //d3.select('.recharts-label-list').attr('fill','#ffffff')
     const { payload } = props;
     if( payload[0].value != 'Null' ){
       value = payload[0].value + '%'
     } else { value = payload[0].value + '*'}
-    return (
-      <div className='custom-tooltip-district'>
-        <text className='descr_value'>{`${(value)}`}</text>
-      </div>
-    );
+    console.log(props)
+    if(props.lastData == payload[0].value && props.lastYear == payload[0]['payload']['Yr']) {
+      return null;
+    } else {
+      return (
+        <div className='custom-tooltip-district'>
+          <text className='descr_value'>{`${(value)}`}</text>
+        </div>
+      );
+    }
   }
-  //lastFill = '#14558f'
   return null;
 }
 
-const CustomActiveDot = (props) => {
-	const { index, cx, cy, fill, r, value} = props
-	return (
-		<Dot cx={cx} cy={cy} r={r} fill={fill} />
-	)
-};
+const CustomizedDot = React.createClass({
+  render () {
+    const {cx, cy, stroke, fill, r, payload, value} = this.props;
+    if (this.props.lastIndex === this.props.index) {
+      return (
+        <Dot cx={cx} cy={cy} stroke={stroke} r={r+2} fill={'#14558f'} />
+      );
+    }
+    return (
+      <Dot cx={cx} cy={cy} r={r} stroke={stroke} fill={'#fff'} />
+    );
+  }
+});
 
 const SimpleLineChart = React.createClass({
 	render () {
@@ -74,6 +89,7 @@ const SimpleLineChart = React.createClass({
     if(data){ 
       for(let i = data.length-1; i > -1; i--) {
         if(data[i][dataKey] != 'Null') {
+          year = data[i]['Yr'];
           value = data[i][dataKey];
           index = i;
           break;
@@ -86,8 +102,8 @@ const SimpleLineChart = React.createClass({
               margin={{top: 15, right: 30, left: -20, bottom: 5}}>
          <XAxis padding={{ left: 0 }} dataKey="Yr" type="number" domain={[2010, 2016]} tick={<CustomizedXAxisTick/>}/>
          <YAxis type="number" domain={[ymin, ymax]} tick={<CustomizedYAxisTick/>}/>
-         <Tooltip content={<CustomTooltip/>}/>
-         <Line type="monotone" dataKey={dataKey} stroke="#14558f" activeDot={<CustomActiveDot />} animationDuration={500} label={<CustomizedLabel lastData={value} lastIndex={index}/>}/>
+         <Tooltip content={<CustomTooltip lastData={value} lastYear={year}/>}/>
+         <Line type="monotone" dataKey={dataKey} stroke="#14558f" dot={<CustomizedDot lastIndex={index}/>} activeDot={<CustomActiveDot lastData={value} lastIndex={index}/>} animationDuration={500} label={<CustomizedLabel lastData={value} lastIndex={index}/>}/>
         </LineChart>
       </ResponsiveContainer>
     );
