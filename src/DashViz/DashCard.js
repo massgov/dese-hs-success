9cbc2f39a4530f30dc9e6a423cbef65feadeb278
Minghua Sun <clairesunstudio@gmail.com>
2017-09-15 16:41:57 -0400

    clean up dash card title render logic

--- src/DashViz/DashCard.js
+++ src/DashViz/DashCard.js
@@ -3,12 +3,9 @@ import Description from '../Description'
 import LineChart from './LineChart'
 import './DashCard.css'
 
-let ymin = 0;
-let ymax = 100; 
-var year, index, value, sentence, sentenceP1, sentenceP2, boldKey, boldKey2;
-
 const DataCard =({Title, data, dataKey, dataCount}) => {
-    	if(data){ 
+      var year, index, value, sentence, studentGroup, cardKey;
+    	if(data){
             console.log(data)
 	      for(let i = data.length-1; i > -1; i--) {
 	        if(data[i][dataKey] != 'Null') {
@@ -16,68 +13,51 @@ const DataCard =({Title, data, dataKey, dataCount}) => {
 	          value = data[i][dataKey];
 	          index = i;
 	          break;
-	        } 
+	        }
       	  }
       	}
-      	if(dataKey){
-    	if ( dataKey === '4 Year Graduation Percent' ){
-    		sentenceP1 = ' of 2015 cohort ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'graduated in 4 years';
-    		ymin = 0;
-    		ymax = 100;
-    	} else if ( dataKey === '5 Year Graduation Percent' ){
-    		sentenceP1 = ' of 2016 cohort ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'graduated in 5 years';
-    		ymin = 0;
-    		ymax = 100;
-    	} else if ( dataKey === 'Mass Core Completion Percent' ){
-    		sentence = ' of 2016 cohort graduated in 5 years in  '
-    		ymin = 0;
-    		ymax = 100;
-    	} else if ( dataKey === 'Chronic Absence Percent' ){
-    		sentenceP1 = ' of high school students ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'missed more than 10% of days';
-    		ymin = 0;
-    		ymax = 100;
-    	} else if ( dataKey === 'Out of School Suspension Percent' ){
-    		sentenceP1 = ' of high school students had ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'at least one out-of-school suspension';
-    		ymin = 0;
-    		ymax = 20;
-    	} else if ( dataKey === 'Passed All 9th Grade Courses Percent' ){
-    		sentenceP1 = ' of 9th graders ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'passed all 9th grade courses';
-    		ymin = 0;
-    		ymax = 100;
-    	} else if ( dataKey === 'Enrolled in AP/IB Course Percent' ){
-    		sentenceP1 = ' of juniors/seniors ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'took at least one AP/IB courses';
-    		ymin = 0;
-    		ymax = 100;
-    	} else if ( dataKey === 'College Enrollment Percent' ){
-    		sentenceP1 = ' of high school students ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'enrolled in college';
-    		ymin = 0;
-    		ymax = 100;
-    	} else if ( dataKey === 'College Persistence Percent' ){
-    		sentenceP1 = ' of high schools students who enrolled in college in 2014 ';
-    		sentenceP2 = ' in ';
-    		boldKey = 'stayed in college';
-    		ymin = 0;
-    		ymax = 100;
+        let ymin = 0, ymax = 100;
+      	switch (dataKey) {
+    	  case '4 Year Graduation Percent':
+      		studentGroup = '2015 cohort';
+      		cardKey = 'graduated in 4 years';
+          break;
+    	  case '5 Year Graduation Percent':
+      		studentGroup = '2016 cohort';
+      		cardKey = 'graduated in 5 years';
+          break;
+    	  case 'Chronic Absence Percent':
+      		studentGroup = 'high school students';
+      		cardKey = 'missed more than 10% of days';
+          break;
+    	  case 'Out of School Suspension Percent':
+      		studentGroup = 'high school students';
+      		cardKey = 'had at least one out-of-school suspension';
+      		ymin = 0;
+      		ymax = 20;
+          break;
+    	  case 'Passed All 9th Grade Courses Percent':
+      		studentGroup = '9th graders';
+      		cardKey = 'passed all 9th grade courses';
+          break;
+      	case 'Enrolled in AP/IB Course Percent':
+      		studentGroup = 'juniors/seniors';
+      		cardKey = 'took at least one AP/IB courses';
+          break;
+      	case 'College Enrollment Percent':
+      		studentGroup = 'high school students';
+      		cardKey = 'enrolled in college';
+          break;
+    	  case 'College Persistence Percent':
+      		studentGroup = 'high school students who enrolled in college in 2014';
+      		cardKey = 'stayed in college';
+          break;
+        default: console.log('data card has not matched')
     	}
-    }
     return(
       <div className="col-md-6">
           <div className="dash-card">
-            <p><strong>{value}%</strong>{sentenceP1}<strong>{boldKey}</strong>{sentenceP2}{year}.</p>
+            <p><strong>{value}%</strong> of {studentGroup} <strong>{cardKey}</strong> in {year}.</p>
             <hr />
             <LineChart data={data} dataKey={dataKey} dataCount={dataCount} ymin={ymin} ymax={ymax}/>
           </div>
