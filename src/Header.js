4a29c6bf3d918f4a951d74fe000c70aeab54a3d4
Minghua Sun <clairesunstudio@gmail.com>
2017-09-21 15:20:21 -0400

    style page header

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,12 @@ class Content extends React.Component{
     return (
       <header className="page-header">
         <div className="container">
-            <h1>High School Success Early Indicators Data Story <br /><small>Massachusetts Department of Elementary & Secondary Education</small></h1>
+          <div className="row">
+            <div className="col-md-8">
+              <h1>The ABCs: Predicting success in high school and beyond<br /></h1>
+              <h3>A data story from the Massachusetts Department of Elementary and Secondary Education (DESE) on how attendance, behavior, and course performance information can forecast student achievement.</h3>
+            </div>
+          </div>
         </div>
       </header>
 
