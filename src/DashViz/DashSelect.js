53c0e1058e5f77664d3bcf8e055e400ef2758090
Minghua Sun <clairesunstudio@gmail.com>
2017-09-25 15:49:11 -0400

    only render back to top link or button scroll into district

--- src/DashViz/DashSelect.js
+++ src/DashViz/DashSelect.js
@@ -5,7 +5,14 @@ import 'react-select/dist/react-select.css';
 import Btn from '../Btn'
 import { Button } from 'react-bootstrap'
 
-
+const BackToTop = () => {
+  return (
+    <div className="backToTop">
+      <Button className="backToTop_btn pull-right" href="#header" aria-label="Back to top"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></Button>
+      <a href="#header" className="backToTop_link pull-right">Back to top</a>
+    </div>
+  )
+}
 class DashSelect extends React.Component{
   constructor(props, context) {
     super(props, context);
@@ -19,7 +26,7 @@ class DashSelect extends React.Component{
     const {options, handleChange, handleClick, value} = this.props
     const { sticky } = this.props
     var fixed
-    if (sticky) { fixed = "navbar-fixed-top"} else {fixed = ""}
+    if (sticky) {fixed = "navbar-fixed-top"} else {fixed = ""}
     return(
       <div className={`dash-select row ${fixed}`}>
         <Btn handleClick={handleClick} value={'State'} selected={value}>View statewide data</Btn>
@@ -30,8 +37,7 @@ class DashSelect extends React.Component{
             options={options}
             onChange={handleChange}
           />
-        <Button bsSize="small" className="backToTop pull-right" href="#header" aria-label="Back to top"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></Button>
-        <a href="#header" className="backToTop_link pull-right">Back to top</a>
+        {(sticky)? <BackToTop /> : <div></div>}
       </div>
     )
   }
