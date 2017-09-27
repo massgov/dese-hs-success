e4ddfaa7fafe068d1dce9c3a6e95ef4524717579
Sienna Svob <sienna.avril.pearl@gmail.com>
2017-09-27 12:44:59 -0400

    updated footnote source references

--- src/FootNote.js
+++ src/FootNote.js
@@ -8,9 +8,9 @@ const FootNote = () => {
     <div className="container">
         <h1 id="footnote-label" className="sr-only">Footnotes</h1>
         <ol>
-            <li id="footnotemsg1"><p>http://www.pbs.org/wgbh/frontline/article/by-the-numbers-dropping-out-of-high-school/ <a href="#footnoteref1"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></a></p></li>
-            <li id="footnotemsg2"><p>https://cew.georgetown.edu/cew-reports/americas-divided-recovery/ <a href="#footnoteref2"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></a></p></li>
-            <li id="footnotemsg3"><p>Core subjects are English/Language Arts, Math, Science and Social Studies. <a href="#footnoteref3"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></a></p></li>
+            <li id="footnotemsg1"><span>http://www.pbs.org/wgbh/frontline/article/by-the-numbers-dropping-out-of-high-school/ </span><a href="#footnoteref1"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></a></li>
+            <li id="footnotemsg2"><span>https://cew.georgetown.edu/cew-reports/americas-divided-recovery/ </span><a href="#footnoteref2"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></a></li>
+            <li id="footnotemsg3"><span>Core subjects are English/Language Arts, Math, Science and Social Studies. </span><a href="#footnoteref3"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></a></li>
             <li id="footnotemsg4"><span>High attendance = 90% and above attendance rate. Low attendance = below 90% attendance rate, which is considered chronically absence.</span> <a href="#footnoteref4"><i className="glyphicon glyphicon-arrow-up" aria-hidden="true" /></a></li>
         </ol>
     </div>
