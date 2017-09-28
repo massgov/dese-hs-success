279e075afeddadf64c6459d385aacd89fa2426a1
Minghua Sun <clairesunstudio@gmail.com>
2017-09-28 14:44:54 -0400

    change mayflower yellow to dese orange

--- src/DataViz/GroupedBarChart.js
+++ src/DataViz/GroupedBarChart.js
@@ -17,7 +17,7 @@ const GroupedBarChart = ({barData}) => {
                             <Tooltip content={<CustomTooltip/>}/>
                             <Legend verticalAlign="top" content={CustomLegend}/>
                             <Bar dataKey="All subjects" fill="#43956f" formatter={toPercent}/>
-                            <Bar dataKey="Core subjects" fill="#f6c51b" formatter={toPercent}/>
+                            <Bar dataKey="Core subjects" fill="#FFB300" formatter={toPercent}/>
                        </BarChart>
                      </ResponsiveContainer>
           );
