6852a4530f37e47c4569da453fd1cf4be776dece
Minghua Sun <clairesunstudio@gmail.com>
2017-09-29 14:14:20 -0400

    add social media share buttons using react-share

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Header.css'
+import SocialMedia from './SocialMedia'
 
 class Content extends React.Component{
-
   render = () =>  {
     return (
       <header className="page-header" id="header" name="header">
@@ -11,6 +11,11 @@ class Content extends React.Component{
             <div className="col-md-8">
               <h1>The ABCs: Predicting success in high school and beyond<br /></h1>
               <h3>A data story from the Massachusetts Department of Elementary and Secondary Education (DESE) on how attendance, behavior, and course performance information can forecast student achievement.</h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-8 ">
+                <SocialMedia />
             </div>
           </div>
         </div>
