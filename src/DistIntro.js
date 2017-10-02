859d3c71825471e6317bcfec0ce50d96e8ab3288
Minghua Sun <clairesunstudio@gmail.com>
2017-10-02 17:03:37 -0400

    style dist intro

--- src/DistIntro.js
+++ src/DistIntro.js
@@ -8,11 +8,10 @@ class Content extends React.Component{
       <div className="container target-nav" id="district" name="district">
         <div className="row">
           <div className="col-md-12">
-            <h2 className="full-width-alt__title"><span>Explore Your District</span></h2>
-            <h3 className="full-width-alt__title"><span>To learn more about the these trends and other information on your school or district:</span></h3>
+            <div className="dist__title"><h2>Explore Your District</h2></div>
             <ul className="icon-list">
-              <li><i className="glyphicon glyphicon-arrow-right list-icon" aria-hidden="true" /> <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">Massachusetts School and District Profiles</a></li>
-              <li><i className="glyphicon glyphicon-arrow-right list-icon" aria-hidden="true" /> <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">Success after High School DART</a></li>
+              <li><i className="glyphicon glyphicon-arrow-right list-icon" aria-hidden="true" />To learn more about the these trends and other information on your school or district: <br />
+              <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">Massachusetts School and District Profiles</a>, <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">Success after High School DART</a></li>
             </ul>
           </div>
         </div>
