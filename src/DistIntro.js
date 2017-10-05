31f7a427ef5617e561e9886bda77e760fb11d4ad
Minghua Sun <clairesunstudio@gmail.com>
2017-10-05 09:51:39 -0400

    update text style in distIntro

--- src/DistIntro.js
+++ src/DistIntro.js
@@ -9,7 +9,7 @@ class Content extends React.Component{
         <div className="row">
           <div className="col-md-12">
             <h2 className="full-width-alt__title"><span>Explore Your District</span></h2>
-            <h4 className="full-width-alt__title"><span>Use the tool below to find ABC data specific to your student’s school district, and see how it compares to the rest of the Commonwealth. To learn even more information about the performance of a school or district, explore DESE’s <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">School and District Profiles</a> and <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">DART</a> resources.</span></h4>
+            <p>Use the tool below to find ABC data specific to your student’s school district, and see how it compares to the rest of the Commonwealth. To learn even more information about the performance of a school or district, explore DESE’s <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">School and District Profiles</a> and <a href="http://profiles.doe.mass.edu/" target="_blank" rel="noopener noreferrer">DART</a> resources.</p>
           </div>
         </div>
       </div>
