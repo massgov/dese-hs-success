767fffd70769950993e360c6d02b32f944c32515
Minghua Sun <clairesunstudio@gmail.com>
2017-10-06 15:25:26 -0400

    add suspension footnote

--- src/FootNote.js
+++ src/FootNote.js
@@ -26,7 +26,7 @@ class FootNotes extends React.Component {
             <FootNote i={3}>Core subjects are English/Language Arts, Math, Science and Social Studies.</FootNote>
             <FootNote i={4}>High attendance means 90% attendance rate or higher. Low attendance means below 90% attendance rate, which is considered chronically absent.</FootNote>
             <FootNote i={5}>Many colleges give college credit to students scoring a 3 or above on an AP Test. Contact your school counselor for more information.</FootNote>
-            <FootNote i={6}>Suspension Rate Footnote.</FootNote>
+            <FootNote i={6}>Suspension rate are suppressed when: 1.There are fewer than 6 students reported. 2. There are fewer than 6 students reported who were disciplined. 3.The number of students disciplined and the types of discipline are suppressed.</FootNote>
           </ol>
       </div>
   </div>
