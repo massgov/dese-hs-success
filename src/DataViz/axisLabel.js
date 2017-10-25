cc9a2296fad4394a53b2c58de01bf86500b4e257
Minghua Sun <clairesunstudio@gmail.com>
2017-10-25 10:06:09 -0400

    fix bar chart label to prevent cut off

--- src/DataViz/axisLabel.js
+++ src/DataViz/axisLabel.js
@@ -3,7 +3,7 @@ import { Text } from 'recharts'
 
 const AxisLabel = ({ axisType, x, y, width, height, stroke, children }) => {
   const isVert = axisType === 'yAxis';
-  const cx = isVert ? x : x + (width / 2);
+  const cx = isVert ? x : (x + width) / 3 + x;
   const cy = isVert ? (height / 2) + y : y + height + 10;
   const rot = isVert ? `270 ${cx} ${cy}` : 0;
   return (
