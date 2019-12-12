530c72292c236deac04d0d37528a881f381d8256
Minghua Sun <clairesunstudio@gmail.com>
2019-12-12 16:59:52 -0500

    output json

--- scripts/csv-to-json.js
+++ scripts/csv-to-json.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const fs = require('fs');
 const csv=require('csvtojson')
 
 const dataPath = path.resolve(__dirname, '../public/data/');
-const filePath = 'district.csv';
+const filePath = 'district';
 
-const csvPath = `${dataPath}/${filePath}`;
+const csvPath = `${dataPath}/${filePath}.csv`;
+const jsonPath = `${dataPath}/${filePath}.json`;
 console.log(csvPath)
 csv()
 .fromFile(csvPath)
 .then((jsonObj)=>{
-    console.log(jsonObj);
+    fs.writeFileSync(jsonPath, JSON.stringify(jsonObj, null, 2), (err) => {
+      if (err) throw err;
+    });
 })
