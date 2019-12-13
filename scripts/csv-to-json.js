ae53f294afbcdb47647e2ebfec301fab936fce61
Minghua Sun <clairesunstudio@gmail.com>
2019-12-13 09:55:09 -0500

    modify script to take in vars

--- scripts/csv-to-json.js
+++ scripts/csv-to-json.js
@@ -2,16 +2,42 @@ const path = require('path');
 const fs = require('fs');
 const csv=require('csvtojson')
 
-const dataPath = path.resolve(__dirname, '../public/data/');
-const filePath = 'district';
+/**
+  * $ node budgets/shared-ui/utils/docs-to-json/generic docsPath jsonPath imgPath fileName
+  * @param {1} script_path_from_root - ./scripts/csv-to-json.js
+  * @param {2} inputPath - path relative to the public/data folder with fileName without file extension
+  e.g. district
+  e.g. 10/grade_10_data
+  * @param {3} outputPath - path relative to the public/data folder with fileName without file extension
+  e.g. district
+  e.g. data
+*/
 
-const csvPath = `${dataPath}/${filePath}.csv`;
-const jsonPath = `${dataPath}/${filePath}.json`;
-console.log(csvPath)
+let inputPath = '';
+let outputPath = '';
+process.argv.forEach((val, index) => {
+  switch (index) {
+    case 2:
+      inputPath = val;
+      break;
+    case 3:
+      outputPath = val;
+      break;
+    default:
+      null
+  }
+});
+
+const dataPath = path.resolve(__dirname, '../public/data/');
+
+const csvPath = `${dataPath}/${inputPath}.csv`;
+const jsonPath = `${dataPath}/${outputPath}.json`;
+
 csv()
 .fromFile(csvPath)
 .then((jsonObj)=>{
     fs.writeFileSync(jsonPath, JSON.stringify(jsonObj, null, 2), (err) => {
       if (err) throw err;
     });
+    console.log(`${outputPath} JSON converted from ${inputPath} CSV`);
 })
