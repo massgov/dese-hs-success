c513301c315500b23ed78a28182bad903be95ea9
Minghua Sun <clairesunstudio@gmail.com>
2019-12-13 13:30:46 -0500

    remove console

--- src/DataViz/toPercent.js
+++ src/DataViz/toPercent.js
@@ -1,7 +1,6 @@
 import numbro from 'numbro';
 
 const toPercent = (number, decimal) => {
-  //console.log(number)
   const mantissa = decimal || 0;
   const percent = numbro(number).format({ output: 'percent', mantissa, spaceSeparated: false });
   return percent;
